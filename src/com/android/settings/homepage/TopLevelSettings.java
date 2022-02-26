sharanpai3/platform_packages_apps_Settings

Settings: Redesign back to old days
Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -133,15 +133,84 @@ public class TopLevelSettings extends DashboardFragment implements
                 icon.setTint(tintColor);
             }
 
-    	    String key = preference.getKey().toString();
-	    if (key.equals("category")){
-		preference.setLayoutResource(R.layout.homepage_category);
-	    } else {
-		preference.setLayoutResource(R.layout.card_view_pref);
-	    }
+	    onSetPrefCard();
+
         }
     }
 
+    private void onSetPrefCard() {
+	final PreferenceScreen screen = getPreferenceScreen();
+        final int count = screen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            final Preference preference = screen.getPreference(i);
+
+ 	    String key = preference.getKey();
+
+	    if (key.equals("top_level_network")){
+	        preference.setLayoutResource(R.layout.card_view_pref_top);
+	    }
+            if (key.equals("top_level_connected_devices")){
+                preference.setLayoutResource(R.layout.card_view_pref_middle);
+            }
+            if (key.equals("top_level_accounts")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+            if (key.equals("top_level_octavi_lab")){
+                preference.setLayoutResource(R.layout.card_view_pref_top);
+            }
+            if (key.equals("top_level_wallpaper")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+            if (key.equals("top_level_battery")){
+                preference.setLayoutResource(R.layout.card_view_pref_top);
+            }
+            if (key.equals("top_level_display")){
+                preference.setLayoutResource(R.layout.card_view_pref_middle);
+            }
+            if (key.equals("top_level_sound")){
+                preference.setLayoutResource(R.layout.card_view_pref_middle);
+            }
+            if (key.equals("top_level_apps")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+            if (key.equals("top_level_storage")){
+                preference.setLayoutResource(R.layout.card_view_pref_top);
+            }
+            if (key.equals("top_level_notification")){
+                preference.setLayoutResource(R.layout.card_view_pref_middle);
+            }
+            if (key.equals("top_level_location")){
+                preference.setLayoutResource(R.layout.card_view_pref_middle);
+            }
+            if (key.equals("top_level_accessibility")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+            if (key.equals("top_level_security")){
+                preference.setLayoutResource(R.layout.card_view_pref_top);
+            }
+            if (key.equals("top_level_privacy")){
+                preference.setLayoutResource(R.layout.card_view_pref_middle);
+            }
+            if (key.equals("top_level_emergency")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+            if (key.equals("top_level_system")){
+                preference.setLayoutResource(R.layout.card_view_pref_top);
+            }
+            if (key.equals("top_level_about_device")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+            if (key.equals("top_level_usercard")){
+                preference.setLayoutResource(R.layout.usercard);
+            }
+            if (key.equals("dashboard_tile_pref_com.google.android.apps.wellbeing.settings.TopLevelSettingsActivity")){
+                preference.setLayoutResource(R.layout.card_view_pref_top);
+            }
+            if (key.equals("dashboard_tile_pref_com.google.android.gms.app.settings.GoogleSettingsIALink")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+	}
+    }
 
     private void onUserCard() {
         final LayoutPreference headerPreference =

XZI-Project/android_packages_apps_Settings

Signed-off-by: Mikazuuu07 <irfn4566@gmail.com>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -133,15 +133,84 @@ public class TopLevelSettings extends DashboardFragment implements
                 icon.setTint(tintColor);
             }
 
-    	    String key = preference.getKey().toString();
-	    if (key.equals("category")){
-		preference.setLayoutResource(R.layout.homepage_category);
-	    } else {
-		preference.setLayoutResource(R.layout.card_view_pref);
-	    }
+	    onSetPrefCard();
+
         }
     }
 
+    private void onSetPrefCard() {
+	final PreferenceScreen screen = getPreferenceScreen();
+        final int count = screen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            final Preference preference = screen.getPreference(i);
+
+ 	    String key = preference.getKey();
+
+	    if (key.equals("top_level_network")){
+	        preference.setLayoutResource(R.layout.card_view_pref_top);
+	    }
+            if (key.equals("top_level_connected_devices")){
+                preference.setLayoutResource(R.layout.card_view_pref_middle);
+            }
+            if (key.equals("top_level_accounts")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+            if (key.equals("top_level_extensions")){
+                preference.setLayoutResource(R.layout.card_view_pref_top);
+            }
+            if (key.equals("top_level_wallpaper")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+            if (key.equals("top_level_battery")){
+                preference.setLayoutResource(R.layout.card_view_pref_top);
+            }
+            if (key.equals("top_level_display")){
+                preference.setLayoutResource(R.layout.card_view_pref_middle);
+            }
+            if (key.equals("top_level_sound")){
+                preference.setLayoutResource(R.layout.card_view_pref_middle);
+            }
+            if (key.equals("top_level_apps")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+            if (key.equals("top_level_storage")){
+                preference.setLayoutResource(R.layout.card_view_pref_top);
+            }
+            if (key.equals("top_level_notification")){
+                preference.setLayoutResource(R.layout.card_view_pref_middle);
+            }
+            if (key.equals("top_level_location")){
+                preference.setLayoutResource(R.layout.card_view_pref_middle);
+            }
+            if (key.equals("top_level_accessibility")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+            if (key.equals("top_level_security")){
+                preference.setLayoutResource(R.layout.card_view_pref_top);
+            }
+            if (key.equals("top_level_privacy")){
+                preference.setLayoutResource(R.layout.card_view_pref_middle);
+            }
+            if (key.equals("top_level_emergency")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+            if (key.equals("top_level_system")){
+                preference.setLayoutResource(R.layout.card_view_pref_top);
+            }
+            if (key.equals("top_level_about_device")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+            if (key.equals("top_level_usercard")){
+                preference.setLayoutResource(R.layout.usercard);
+            }
+            if (key.equals("dashboard_tile_pref_com.google.android.apps.wellbeing.settings.TopLevelSettingsActivity")){
+                preference.setLayoutResource(R.layout.card_view_pref_top);
+            }
+            if (key.equals("dashboard_tile_pref_com.google.android.gms.app.settings.GoogleSettingsIALink")){
+                preference.setLayoutResource(R.layout.card_view_pref_bottom);
+            }
+	}
+    }
 
     private void onUserCard() {
         final LayoutPreference headerPreference =